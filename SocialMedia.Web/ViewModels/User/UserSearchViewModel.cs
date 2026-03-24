5c58c09c9d20ede4a5bae1554d451a6790b23e4a
Ahmed Hamdy <ahmedaltahtawy01@gmail.com>
2026-03-24 21:32:02 +0200

    feat(search): add search view models for user search feature

---
+++ SocialMedia.Web/ViewModels/User/UserSearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialMedia.Web.ViewModels.User
+{
+    public class UserSearchViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string? ProfileImage { get; set; }
+
+    }
+}
